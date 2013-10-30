60bef80764e05c78c12d9102350e1c51e1fc76ad
Magnus Tønsager <magnustonsager@gmail.com>
2013-10-30 21:51:15 +0100

    Create index.php
    Startet på htmlen for siden

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hjemmeside</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" title="styleen" href="">
+
+	</head>
+	<body>
+		<header>
+			<img src="" alt"Logo">
+			<h1>
+				Hjemmeside
+			</h1>
+		</header>
+		<nav>
+			<div class="nav">Underside</div>
+			<div class="nav">Underside</div>
+			<div class="nav">Underside</div>
+			<div class="nav">Underside</div>
+		</nav>
+
+		<section id="innhold">
+			<?php>
+
+			</?>
+		</section>
+
+		<footer>
+			<p>Laget av gruppe 41</p>
+		</footer>
+	</body>
+</html>
