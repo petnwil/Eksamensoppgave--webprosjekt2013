03571be28a2c5d6679f9edd4c4e654458d693ef3
Magnus Tønsager <magnustonsager@gmail.com>
2013-11-07 19:32:47 +0100

    script for vis mer knapper

---
+++ script.js
@@ -0,0 +1,14 @@
+/*Script for vis mer knapper, krever class="mindre" i htmlen på vis mindre knappene.
+Det som skal vises/skjules må ligge i en article tagg.*/
+
+$(document).ready(function(){
+	//$("button").css("display", "inline");
+	$(".mindre").toggle();
+	$("article").toggle();
+
+	$("button").click(function(){
+		$(this).siblings("button").toggle();
+		$(this).toggle();
+		$(this).siblings("article").toggle();
+	});
+});
