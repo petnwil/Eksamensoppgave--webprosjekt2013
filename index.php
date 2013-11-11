f3f8c6a4a6aa11b397ae01233947930b2bed65c9
petnwil <PeterWilhelmsen@pptp-145-24.vpn.hio.no>
2013-11-11 23:11:20 +0100

    css og index.php der "hjem.html" er inkludert

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+$page = $_GET['page'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hjemmeside</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" title="styleen" href="style.css">
+
+</head>
+
+	<header id="header">
+		<img src="baflogo.jpg" alt="Logo" title="Logo">
+		<h1>
+			BI Athletics Fotball
+		</h1>
+
+	<nav>
+		<ul id="meny">
+			<li> <a href="index.php"> Hjem </a> </li>
+			<li> <a href="#"> Lag </a>
+				<ul>
+					<li> <a href="index.php?page=1"> Herre </a> </li>
+					<li> <a href="#"> Dame </a> </li>						
+				</ul>
+			</li>
+			<li> <a href="#"> Profiler </a> </li>
+			<li> <a href="#"> Media </a>
+				<ul>
+					<li> <a href="#"> Bilder </a> </li>
+					<li> <a href="#"> Video </a> </li>				
+				</ul>
+			</li>	
+			<li> <a href="#"> Kontakt </a> </li>
+			<li> <a href="#"> Søkefelt </li>		
+		</ul>
+	</nav>
+
+</header>
+<br>
+<body>
+	<section id="innholdet">
+	<?php
+	switch($page){
+
+		case "1":
+		include('underside1.html');
+			break;
+
+			case "2":
+			include('underside2.html');
+				break;
+
+				case "3":
+				include('underside3.html');
+				break;
+
+				default: include('hjem.html');
+				break;
+			}
+
+			?>
+	</section>
+			<footer>
+				<p>Laget av gruppe 41</p>
+			</footer>
+		</body>
+		</html>
