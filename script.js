6a17051e1c304b0c0017d9ab8a6b27a6dd37db45
Magnus Tønsager <magnustonsager@gmail.com>
2013-11-18 14:16:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-/*Script for vis mer knapper, krever class="mindre" i htmlen på vis mindre knappene.
-Det som skal vises/skjules må ligge i en article tagg.*/
+/*Script for vis mer knapper, krever class="mindre" i htmlen pÃ¥ vis mindre knappene.
+Det som skal vises/skjules mÃ¥ ligge i en article tagg.*/
 
 $(document).ready(function(){
-	$("button").css("display", "inline");
+	$("button").css("display", "block");
 	$(".mindre").toggle();
 	$("article").toggle();
 
 	$("button").click(function(){
 		$(this).siblings("button").toggle();
 		$(this).toggle();
-		$(this).siblings("article").toggle();
+		$(this).siblings("article").toggle(500);
+
+		if( $(this).attr("class") == "mindre")
+		{
+			$(this).siblings("img").css({
+				height: "100px",
+				width: "auto"
+			});
+		}
+		else
+		{
+			$(this).siblings("img").css({
+				height: "300px",
+				width: "auto"
+			});
+		}
 	});
+
 });
